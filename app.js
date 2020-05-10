c8c6edafd3eba25dfb731d1c5eec2ab5821ec09a
Eddie Rosas <eddie_rosas@outlook.com>
2020-05-10 14:39:54 -0700

    commit everything, not just frontend

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const db = require('./config/keys').mongoURI;
-
-
-app.get("/", (req, res) => res.send("cahnge"));
+const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const passport = require("passport");
 
 mongoose
   .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
-  .catch((err) => console.log(err));
+  .catch(err => console.log(err));
 
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
