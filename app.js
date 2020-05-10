bd7edfd2e9970b048b4e9b247e4850d228070026
Eddie Rosas <eddie_rosas@outlook.com>
2020-05-10 14:56:47 -0700

    attempt heroku push

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use("/api/users", users)
 app.use("/api/tweets", tweets)
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
@@ -31,6 +31,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
