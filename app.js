df9d77cb80fa0e99c0c723986f34a812dc6ab867
Eddie Rosas <eddie_rosas@outlook.com>
2020-05-10 20:46:54 -0700

    reconfigure vars

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(bodyParser.json());
 app.use("/api/users", users)
 app.use("/api/tweets", tweets)
 
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
@@ -30,7 +32,4 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
-
-// app.listen(port, () => console.log(`Server is running on port ${port}`));
-
 
