66f4f9c7fb93ff012bdcce07d639bf24a67d6220
EddieRosas <60726970+EddieRosas@users.noreply.github.com>
2020-05-10 23:38:02 -0700

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev0026:CotakkRj8Xr6aUKm@cluster0-byqmu.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "mT5zi0*_8aH<HVn",
-  //Make sure this is your own unique string
-};
